4b3cc1766521e0917dc0b60253ce5e25bd778942
Shaun Kulesa <shaunkulesa@gmail.com>
2022-11-10 12:41:11 +0000

    table updates

--- table.py
+++ table.py
@@ -1,4 +1,8 @@
-from tkinter import *            
+from tkinter import *
+import time
+
+
+start = time.perf_counter()
 
 class Header:
     def __init__(self, table, data=[], outline_color='black', fill_color='white', outline_width=0):
@@ -8,8 +12,6 @@ class Header:
         self.outline_color = outline_color
         self.fill_color = fill_color
         self.outline_width = outline_width
-
-        # self.height = 30
     
     def get_name(self):
         return self.table_name
@@ -19,7 +21,6 @@ class Header:
     
     def get_text_width(self, column):
         text = Label(text=self.data[column])
-        print(self.data[column])
         return text.winfo_reqwidth()
     
     def get_height(self):
@@ -56,7 +57,7 @@ class Table:
     def __init__(self, table_name):
         self.records = []
         self.canvas = Canvas()
-        self.canvas.pack(anchor='n', fill='both', expand=True)
+        self.canvas.pack(anchor='center')
         self.table_name = table_name
 
         self.column_widths = []
@@ -74,27 +75,46 @@ class Table:
         for column_counter in range(len(self.columns)):
             widths = []
             widths.append(self.header.get_text_width(column_counter))
-            print(widths)
             for record in self.records:
                 widths.append(record.get_text_width(column_counter))
             self.column_widths.append(max(widths))
         record_width = sum(self.column_widths)
 
-        # #draw
-        # self.canvas.config(width=record_width, height=self.header.get_height() + (self.header.get_height() * len(self.records)))
-        self.canvas.create_rectangle(0, 0, record_width, self.header.get_height(), fill=self.header.fill_color, outline=self.header.outline_color, width=self.header.outline_width)
+        header_height = self.header.get_height()
 
-        previous_column_width = -5
+        self.canvas.create_rectangle(0, 0, record_width, header_height + (30 * len(self.records)), fill=self.header.fill_color, outline=self.header.outline_color, width=self.header.outline_width)
+
+        previous_column_width = -3
+
+        self.canvas.config(width=record_width, height=header_height + (header_height * len(self.records)))
+
+        self.canvas.create_line(3, header_height, record_width, header_height, fill=self.header.outline_color, width=self.header.outline_width)
         for i in range(len(self.columns)):
             previous_column_width += (self.column_widths[i])
-            self.canvas.create_text(previous_column_width, self.header.get_height() / 2, text=self.columns[i], anchor='e')
-
+            self.canvas.create_text(previous_column_width, header_height / 2, text=self.columns[i], anchor='e')
+            self.canvas.create_line(previous_column_width + 3, 0, previous_column_width + 3, header_height + (30 * len(self.records)), fill='#D3D3D3', width=self.header.outline_width)
+        
+        #create record text
+        for record_counter in range(len(self.records)):
+            previous_column_width = -3
+            #create record
+            self.canvas.create_line(0, (header_height * (record_counter + 1)), record_width, (header_height * (record_counter + 1)), fill="#D3D3D3", width=self.header.outline_width)
+            for column_counter in range(len(self.columns)):
+                previous_column_width += (self.column_widths[column_counter])
+                self.canvas.create_text(previous_column_width - (self.column_widths[column_counter] / 2), (header_height * (record_counter + 1)) + (header_height / 2), text=self.records[record_counter].data[column_counter], anchor='center')
 root = Tk()
 
 table = Table('Person')
 header = Header(table, ['firstName', 'lastName', 'Age'])
 table.add_header(header)
-# table.add_records([Record(table, ['Shaun', 'Kulesa', 17,])] * 10000)
+table.add_records([Record(table, ['Shaun', 'Kulesa', 17,])] * 10)
+table.add_records([Record(table, ['John', 'Doe', 20,])] * 10)
+
 table.draw()
+
+
+        
+finish = time.perf_counter()
+print(f'Finished in {round(finish-start, 2)} second(s)')
 
 mainloop()
