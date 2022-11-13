2f2b06b12b889b22a7a72d309af1d565195fde85
cid0rz <cid.kampeador@gmail.com>
2022-11-13 21:11:01 +0100

    naming convention to fields and records

--- database_backend.py
+++ database_backend.py
@@ -28,37 +28,42 @@ class SqliteHandler:
         print(tables)
 
 
-    def add_rows(self, table, data):
+    def add_records(self, table, data):
+        table = ''.join( chr for chr in table if chr.isalnum() )
         values = ", ".join(data)
         self.cur.execute(f"INSERT INTO {table} VALUES {values}")
         self.con.commit()
 
-    def get_all_rows(self, table):
-
-        self.cur.execute(f"SELECT rowid, * FROM {table};")
+    def get_all_records(self, table):
+        table = ''.join( chr for chr in table if chr.isalnum() )
+        self.cur.execute(f"SELECT rowid, * FROM {table} ;")
         return [i for i in enumerate(self.cur.fetchall())]
 
-    def delete_row(self, table, pos):
-        row_id = self.get_all_rows(table)[pos][1][0]
+    def delete_record(self, table, pos):
+        row_id = self.get_all_records(table)[pos][1][0]
+        table = ''.join( chr for chr in table if chr.isalnum() )
         self.cur.execute(f"DELETE FROM {table} WHERE rowid={row_id}")
         self.con.commit()
 
-    def get_row(self, table, pos):
-        return self.get_all_rows(table)[pos]    
+    def get_record(self, table, pos):
+        return self.get_all_records(table)[pos]    
 
     def get_fields(self, table):
+        table = ''.join( chr for chr in table if chr.isalnum() )
         self.cur.execute(f"PRAGMA table_xinfo({table})")
         return [(i[0], i[1]) for i in self.cur.fetchall()]
 
-    def edit_row(self, table, pos, data):
+    def edit_record(self, table, pos, data):
+        table = ''.join( chr for chr in table if chr.isalnum() )
         fields = self.get_fields(table)
-        row_id = self.get_all_rows(table)[pos][1][0]
+        row_id = self.get_all_records(table)[pos][1][0]
         if len(data) != len(fields):
             raise ValueError("incorrect number of fields")
-        update = ", ".join([f"{field[1].upper()}={newval}" for field, newval in zip(fields, data) if data != None])
-        print(f"""UPDATE TABLE '{table}' SET {update} WHERE rowid={row_id}""")
-        self.cur.execute(f"""UPDATE TABLE '{table}' SET {update} WHERE rowid={row_id}""")
+        update = ", ".join([f"{field[1].upper()}={newval}" for field, newval in zip(fields, data) if newval!=None])
+        print(f"""UPDATE '{table}' SET {update} WHERE rowid={row_id}""")
+        self.cur.execute(f"""UPDATE '{table}' SET {update} WHERE rowid={row_id}""")
         self.con.commit()
+        return True
 
 
 with SqliteHandler("./database.db") as sql:
@@ -66,17 +71,18 @@ with SqliteHandler("./database.db") as sql:
     #sql.add_table("movie", ["title", "year", "score"])
     #sql.add_table("actors", ["age", "oscars", "IQ", "cache"])
     sql.list_tables()
-    #sql.add_rows("movie", ["""("Monty Python and the Holy Grail", 1975, 8.2)""",
+    #sql.add_records("movie", ["""("Monty Python and the Holy Grail", 1975, 8.2)""",
     #                          """("And Now for Something Completely Different", 1971, 7.5)""",
     #                          """("Monty Python Live at the Hollywood Bowl", 1982, 7.9)""",
     #                          """("Monty Python's The Meaning of Life", 1983, 7.5)""",
     #                          """("Monty Python's Life of Brian", 1979, 8.0)"""])
-    #sql.add_rows("actors", ["""("Tom Cruise", 2, -10, "2M")"""])
+    #sql.add_records("actors", ["""("Tom Cruise", 2, -10, "2M")"""])
     #sql.cur.execute("SELECT score FROM movie").fetchall()
-    for record in sql.get_all_rows('movie'):
+    for record in sql.get_all_records('movie'):
         print(record)
     #sql.delete_row("movie", 0)
     print(sql.get_fields('movie'))
-    print(sql.get_row('movie', 10))
-    print(sql.edit_row('movie', 10, ("edited title", 9999, 0)))
-    print(sql.get_row('movie', 10))
+    print(sql.get_record('movie', 10))
+    print(sql.edit_record('movie', 10, ("'edited title'", None, 0)))
+    #sql.delete_record('movie', 10)
+    print(sql.get_record('movie', 10))
