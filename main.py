08a856a330299e50117b7eb1d4dc88678ddeaf48
Shaun Kulesa <69469909+ShaunKulesa@users.noreply.github.com>
2022-12-14 22:14:47 +0000

    sizing issue

--- main.py
+++ main.py
@@ -30,27 +30,29 @@ class MainFrame(Frame):
         m1 = PanedWindow(orient=VERTICAL)
         m1.pack(fill=BOTH)
 
-        top_frame = Frame(m1, height=600 * 0.20, width=800, relief=SUNKEN, bg="red")
+        top_frame = Frame(m1, height=600 * 0.20, width=800, relief=SUNKEN)
         m1.add(top_frame)
 
         open_file_button = Button(top_frame, text="Open File", command=lambda: self.open_file())
         open_file_button.pack()
 
         m2 = PanedWindow(m1, orient=HORIZONTAL)
+        m2.pack(fill=Y, expand=True)
         m1.add(m2)
 
-        left_frame = Frame(m2, height=600, width=800 * 0.20, relief=SUNKEN, bg="blue")
+        left_frame = Frame(m2, width=800 * 0.20, relief=SUNKEN)
+        left_frame.pack(fill=BOTH, expand=True)
         m2.add(left_frame)
 
         self.table_explorer = ttk.Treeview(left_frame)
         self.table_explorer.pack()
 
-        self.middle_frame = Frame(m2, height=600, width=800 * 0.60, relief=SUNKEN, bg="green")
+        self.middle_frame = Frame(m2, relief=SUNKEN)
+        self.middle_frame.pack(fill=BOTH, expand=True)
         m2.add(self.middle_frame)
     
     def open_file(self):
-        file_explorer = filedialog.askopenfilename(initialdir="/", title="Select File", filetypes=(("DB Files", "*.db"), ("All Files", "*.*")))
-        self.database_path = file_explorer
+        self.database_path = filedialog.askopenfilename(initialdir="/", title="Select File", filetypes=(("DB Files", "*.db"), ("All Files", "*.*")))
 
         with SqliteHandler(self.database_path) as sql:
             tables = sql.list_tables()
@@ -59,14 +61,12 @@ class MainFrame(Frame):
 
             self.table_explorer.bind("<<TreeviewSelect>>", self.load_table)
 
-
             for table in tables:
                 self.table_explorer.insert('', 'end', text=table, iid=table)
 
                 for field in sql.get_fields(table):
                     self.table_explorer.insert(table, 'end', text=field, iid=f'{table}-?!£$%^&*{field}')
         
-    
     def load_table(self, event):
         self.table = Table(self.middle_frame, self.database_path.split("/")[-1].split(".")[0])
 
@@ -74,19 +74,14 @@ class MainFrame(Frame):
             table = self.table_explorer.focus()
             table = table.split("-?!£$%^&*")[0]
 
-            print(table)
-
-            print(sql.get_fields(table))
             header = Header(self.table, sql.get_fields(table))
             self.table.add_header(header)
 
             for record in sql.get_all_records(table):
                 self.table.add_records([Record(self.table, record[1])])
 
-            # print(sql.get_all_records(table))
-
-
         self.table.draw()
+        self.table.frame.pack(side=LEFT, fill=BOTH, expand=True)
 
 window = Window(MainFrame, 800, 600, None)
 mainloop()
